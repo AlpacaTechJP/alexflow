7b7b9d93e3953b151f819dec8405931af54a8a29
yoshiso <nya060@gmail.com>
2020-10-09 15:21:54 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="alexflow",
-    version="1.1.2",
+    version="1.1.3",
     description="ALEXFlow is a python workflow library built for reproducible complex workflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
